38fc2edd57865684580de312c701082256969f54
Richard Nader, Jr <rmnader@svsu.edu>
2021-09-11 20:19:27 -0400

    Added permissions support. Fixes #2.

--- src/main/java/pw/twpi/whitelistsync2/commands/CommandOp.java
+++ src/main/java/pw/twpi/whitelistsync2/commands/CommandOp.java
@@ -12,12 +12,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class CommandOp implements CommandExecutor {
 
-
     private JavaPlugin plugin;
     private BaseService service;
     private Server server;
 
-    private final String USAGE_STRING = "/wlop <list|op|deop|sync|copyServerToDatabase>";
+    private final String WL_OP_MANAGE = "whitelistsync2.wlop.manage";
+    private final String WL_OP_VIEW = "whitelistsync2.wlop.view";
 
     public CommandOp(JavaPlugin plugin, BaseService service) {
         this.plugin = plugin;
@@ -25,101 +25,113 @@ public class CommandOp implements CommandExecutor {
         this.server = plugin.getServer();
     }
 
-
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        if (plugin.getConfig().getBoolean("general.sync-ops")) {
+            if (args.length > 0) {
+                //Action for showing list
+                if (args[0].equalsIgnoreCase("list")) {
 
-        // Basic permissions
-        if(sender.isOp()) {
-            if (plugin.getConfig().getBoolean("general.sync-ops")) {
-                if (args.length > 0) {
-                    //Action for showing list
-                    if (args[0].equalsIgnoreCase("list")) {
+                    if(!sender.hasPermission(WL_OP_VIEW)) {
+                        sender.sendMessage("You do not have permission to use this command.");
+                        return true;
+                    }
 
-                        sender.sendMessage(Utilities.FormatOppedPlayersOutput(service.getOppedPlayersFromDatabase()));
+                    sender.sendMessage(Utilities.FormatOppedPlayersOutput(service.getOppedPlayersFromDatabase()));
 
-                    } // Actions for adding a player to whitelist
-                    else if (args[0].equalsIgnoreCase("op")) {
+                    return true;
+                } else if (args[0].equalsIgnoreCase("op")) {
+                    // Actions for adding a player to whitelist
+                    if(!sender.hasPermission(WL_OP_MANAGE)) {
+                        sender.sendMessage("You do not have permission to use this command.");
+                        return true;
+                    }
 
-                        if (args.length > 1) {
+                    if (args.length > 1) {
 
-                            OfflinePlayer player = Bukkit.getOfflinePlayer(args[1]);
+                        OfflinePlayer player = Bukkit.getOfflinePlayer(args[1]);
 
-                            if (player != null) {
+                        if (player != null) {
 
-                                if (service.addOppedPlayer(player)) {
-                                    player.setOp(true);
-                                    sender.sendMessage(player.getName() + " opped!");
-                                } else {
-                                    sender.sendMessage("Error opping " + player.getName() + "!");
-                                }
-
+                            if (service.addOppedPlayer(player)) {
+                                player.setOp(true);
+                                sender.sendMessage(player.getName() + " opped!");
                             } else {
-                                sender.sendMessage("User " + args[1] + " not found!");
+                                sender.sendMessage("Error opping " + player.getName() + "!");
                             }
 
                         } else {
-                            sender.sendMessage("You must specify a name to op!");
-                        }
-
-                    } // Actions for removing player from whitelist
-                    else if (args[0].equalsIgnoreCase("deop")) {
-
-                        if (args.length > 1) {
-
-                            OfflinePlayer player = Bukkit.getOfflinePlayer(args[1]);
-
-                            if (player != null) {
-
-                                if (service.removeOppedPlayer(player)) {
-                                    player.setOp(false);
-                                    sender.sendMessage(player.getName() + " de-opped!");
-                                } else {
-                                    sender.sendMessage("Error de-opping " + player.getName() + "!");
-                                }
-
-                            } else {
-                                sender.sendMessage("User " + args[1] + " not found!");
-                            }
-
-                        } else {
-                            sender.sendMessage("You must specify a valid name to deop!");
-                        }
-
-                    } else if (args[0].equalsIgnoreCase("sync")) {
-
-                        if (service.copyDatabaseOppedPlayersToLocal(server)) {
-                            sender.sendMessage("Local up to date with database!");
-                        } else {
-                            sender.sendMessage("Error syncing local to database!");
-                        }
-
-                    } // Sync server to database
-                    else if (args[0].equalsIgnoreCase("copyservertodatabase")) {
-
-                        if (service.copyLocalOppedPlayersToDatabase()) {
-                            sender.sendMessage("Pushed local to database!");
-                        } else {
-                            sender.sendMessage("Error pushing local to database!");
+                            sender.sendMessage("User " + args[1] + " not found!");
                         }
 
                     } else {
-                        return false;
+                        sender.sendMessage("You must specify a name to op!");
                     }
-                } else {
-                    return false;
+
+                    return true;
+                } else if (args[0].equalsIgnoreCase("deop")) {
+                    // Actions for removing player from whitelist
+                    if(!sender.hasPermission(WL_OP_MANAGE)) {
+                        sender.sendMessage("You do not have permission to use this command.");
+                        return true;
+                    }
+
+                    if (args.length > 1) {
+
+                        OfflinePlayer player = Bukkit.getOfflinePlayer(args[1]);
+
+                        if (player != null) {
+
+                            if (service.removeOppedPlayer(player)) {
+                                player.setOp(false);
+                                sender.sendMessage(player.getName() + " de-opped!");
+                            } else {
+                                sender.sendMessage("Error de-opping " + player.getName() + "!");
+                            }
+
+                        } else {
+                            sender.sendMessage("User " + args[1] + " not found!");
+                        }
+
+                    } else {
+                        sender.sendMessage("You must specify a valid name to deop!");
+                    }
+
+                    return true;
+                } else if (args[0].equalsIgnoreCase("sync")) {
+                    // Sync server to database
+                    if(!sender.hasPermission(WL_OP_MANAGE)) {
+                        sender.sendMessage("You do not have permission to use this command.");
+                        return true;
+                    }
+
+                    if (service.copyDatabaseOppedPlayersToLocal(server)) {
+                        sender.sendMessage("Local up to date with database!");
+                    } else {
+                        sender.sendMessage("Error syncing local to database!");
+                    }
+
+                    return true;
+                } else if (args[0].equalsIgnoreCase("copyservertodatabase")) {
+                    // Sync server to database
+                    if(!sender.hasPermission(WL_OP_MANAGE)) {
+                        sender.sendMessage("You do not have permission to use this command.");
+                        return true;
+                    }
+
+                    if (service.copyLocalOppedPlayersToDatabase()) {
+                        sender.sendMessage("Pushed local to database!");
+                    } else {
+                        sender.sendMessage("Error pushing local to database!");
+                    }
+
+                    return true;
                 }
-            } else {
-                sender.sendMessage("Whitelist Sync Op management is not enabled. You must enable it in the config.");
-                return false;
             }
         } else {
-            sender.sendMessage("You must be an op to use this command.");
-            return false;
+            sender.sendMessage("Whitelist Sync Op management is not enabled. You must enable it in the config.");
         }
 
-
-
-        return true;
+        return false;
     }
 }
